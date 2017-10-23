bd9f065dcc7af748836d902930e65794c6d03884
ArmstrongWall <160111223wzq@gmail.com>
2017-10-23 15:36:57 +0800

    path find and display

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,17 @@
 #include <GL/glut.h>
 #include <unistd.h>
+#include "stlastar.h" // See header for copyright and usage information
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+#define DEBUG_LISTS 0
+#define DEBUG_LIST_LENGTHS_ONLY 0
+
+using namespace std;
+
+// Global data
 
 // The world map
 
@@ -53,48 +65,367 @@ int GetMap( int x, int y )
 }
 
 
+// Definitions
+
+class MapSearchNode
+{
+public:
+	int x;	 // the (x,y) positions of the node
+	int y;	
+	
+	MapSearchNode() { x = y = 0; }
+	MapSearchNode( int px, int py ) { x=px; y=py; }
+
+	float GoalDistanceEstimate( MapSearchNode &nodeGoal );
+	bool IsGoal( MapSearchNode &nodeGoal );
+	bool GetSuccessors( AStarSearch<MapSearchNode> *astarsearch, MapSearchNode *parent_node );
+	float GetCost( MapSearchNode &successor );
+	bool IsSameState( MapSearchNode &rhs );
+
+	void PrintNodeInfo();
+    //void grid_map_display(void);
+
+};
+
+bool MapSearchNode::IsSameState( MapSearchNode &rhs )
+{
+//judge is SameState
+	// same state in a maze search is simply when (x,y) are the same
+	if( (x == rhs.x) &&
+		(y == rhs.y) )
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+void MapSearchNode::PrintNodeInfo()
+{
+	char str[100];
+	sprintf( str, "Node position : (%d,%d)\n", x,y );
+      
+
+	cout << str;
+    
+    float leftupper_coor = 0.0,rightdown_coor = 0.0;
+    
+    leftupper_coor = -1 + x*RATIO_WIDTH;
+    rightdown_coor =  1 - y*RATIO_HEIGHT;
+    glColor3ub(152 ,245 ,255);
+    glRectf(leftupper_coor,rightdown_coor,leftupper_coor+RATIO_WIDTH,rightdown_coor-RATIO_HEIGHT);
+    
+    glFlush();
+    
+    //usleep(1000*100);
+}
+
+// Here's the heuristic function that estimates the distance from a Node
+// to the Goal. 
+
+float MapSearchNode::GoalDistanceEstimate( MapSearchNode &nodeGoal )
+{//manhattan distance
+	return fabsf(x - nodeGoal.x) + fabsf(y - nodeGoal.y);	
+}
+
+bool MapSearchNode::IsGoal( MapSearchNode &nodeGoal )
+{
+
+	if( (x == nodeGoal.x) &&
+		(y == nodeGoal.y) )
+	{
+		return true;
+	}
+
+	return false;
+}
+
+// This generates the successors to the given Node. It uses a helper function called
+// AddSuccessor to give the successors to the AStar class. The A* specific initialisation
+// is done for each node internally, so here you just set the state information that
+// is specific to the application
+bool MapSearchNode::GetSuccessors( AStarSearch<MapSearchNode> *astarsearch, MapSearchNode *parent_node )
+{
+
+	int parent_x = -1; 
+	int parent_y = -1; 
+
+	if( parent_node )
+	{
+		parent_x = parent_node->x;
+		parent_y = parent_node->y;
+	}
+	
+
+	MapSearchNode NewNode;
+
+	// push each possible move except allowing the search to go backwards
+
+	if( (GetMap( x-1, y ) < 9) 
+		&& !((parent_x == x-1) && (parent_y == y))
+	  ) 
+	{
+		NewNode = MapSearchNode( x-1, y );
+		astarsearch->AddSuccessor( NewNode );
+	}	
+
+	if( (GetMap( x, y-1 ) < 9) 
+		&& !((parent_x == x) && (parent_y == y-1))
+	  ) 
+	{
+		NewNode = MapSearchNode( x, y-1 );
+		astarsearch->AddSuccessor( NewNode );
+	}	
+
+	if( (GetMap( x+1, y ) < 9)
+		&& !((parent_x == x+1) && (parent_y == y))
+	  ) 
+	{
+		NewNode = MapSearchNode( x+1, y );
+		astarsearch->AddSuccessor( NewNode );
+	}	
+
+		
+	if( (GetMap( x, y+1 ) < 9) 
+		&& !((parent_x == x) && (parent_y == y+1))
+		)
+	{
+		NewNode = MapSearchNode( x, y+1 );
+		astarsearch->AddSuccessor( NewNode );
+	}	
+
+	return true;
+}
+
+// given this node, what does it cost to move to successor. In the case
+// of our map the answer is the map terrain value at this node since that is 
+// conceptually where we're moving
+
+float MapSearchNode::GetCost( MapSearchNode &successor )
+{
+	return (float) GetMap( x, y );
+
+}
+
 void grid_map_display(void)
 {
     int width =0 , height= 0;
     float leftupper_coor = 0.0,rightdown_coor = 0.0;
-    glClear(GL_COLOR_BUFFER_BIT);
+   
+
     for(width = 0; width < MAP_WIDTH; width++)
     {
         for(height = 0; height < MAP_HEIGHT; height++)
         {
             leftupper_coor = -1 + width*RATIO_WIDTH;
             rightdown_coor =  1 - height*RATIO_HEIGHT;
-            if(GetMap(width,height)==1)
+            if(GetMap(width,height)==1)//pass
             {
-                glColor3f(0.5f, 0.6f, 0.4f);
+                glColor3ub(0 ,255 ,127);//green
                 glRectf(leftupper_coor,rightdown_coor,leftupper_coor+RATIO_WIDTH,rightdown_coor-RATIO_HEIGHT);
             }
-            else if(GetMap(width,height)==9)
+            else if(GetMap(width,height)==9)//barrior
             {
-                glColor3f(0.0f, 0.3f, 0.5f);
+                glColor3ub(250, 128 ,114);//red
                 glRectf(leftupper_coor,rightdown_coor,leftupper_coor+RATIO_WIDTH,rightdown_coor-RATIO_HEIGHT);
             }
             
-            glFlush();
+            
         }
-        
            
     }
-   
-            
+      
+     glFlush();        
 
-    return;
 }
 
-int main(int argc, char *argv[])
+void Display_grid(void)
 {
+    int width =0 , height= 0;
+    
+    glLineWidth(1.5f); //ÉèÖÃÏß¿í
+    glColor3ub(0 ,0 ,0);
+    
+    for(width = 0; width <= MAP_WIDTH ; width++)
+    {
+        glBegin(GL_LINES);
+            glVertex2f(-1 + width*RATIO_WIDTH, 1.0f);
+            glVertex2f(-1 + width*RATIO_WIDTH, -1.0f);
+        glEnd();
+    }
+    for(height = 0; height <= MAP_HEIGHT ; height++)
+    {
+        glBegin(GL_LINES);
+            glVertex2f(-1.0f, 1 - height*RATIO_HEIGHT);
+            glVertex2f( 1.0f, 1 - height*RATIO_HEIGHT);
+        glEnd();
+    }
+    
+   
+    glFlush();  
+}
+
+
+
+// Main
+
+int main( int argc, char *argv[] )
+{
+
+	cout << "STL A* Search implementation\n(C)2001 Justin Heyes-Jones\n";
+
+	// Our sample problem defines the world as a 2d array representing a terrain
+	// Each element contains an integer from 0 to 5 which indicates the cost 
+	// of travel across the terrain. Zero means the least possible difficulty 
+	// in travelling (think ice rink if you can skate) whilst 5 represents the 
+	// most difficult. 9 indicates that we cannot pass.
+
+	// Create an instance of the search class...
+
+	AStarSearch<MapSearchNode> astarsearch;
+
+	unsigned int SearchCount = 0;
+
+	const unsigned int NumSearches = 1;
+    
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowPosition(0, 0); 
-    glutInitWindowSize(300, 300);
+    glutInitWindowPosition(400, 100);
+    glutInitWindowSize(900, 900);
     glutCreateWindow("OpenGL 3D View");
-    glutDisplayFunc(grid_map_display);
-    glutMainLoop();
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    grid_map_display();
+    
 
-    return 0;
+	while(SearchCount < NumSearches)
+	{
+
+		// Create a start state
+		MapSearchNode nodeStart;
+		nodeStart.x = rand()%MAP_WIDTH;
+		nodeStart.y = rand()%MAP_HEIGHT; 
+        
+		// Define the goal state
+		MapSearchNode nodeEnd;
+		nodeEnd.x = rand()%MAP_WIDTH;						
+		nodeEnd.y = rand()%MAP_HEIGHT; 
+		
+        cout << "nodeStart.x " << nodeStart.x << endl
+		<< "nodeStart.y " << nodeStart.y << endl;
+		// Set Start and goal states
+		
+		astarsearch.SetStartAndGoalStates( nodeStart, nodeEnd );
+
+		unsigned int SearchState;
+		unsigned int SearchSteps = 0;
+
+		do
+		{
+			SearchState = astarsearch.SearchStep();
+
+			SearchSteps++;
+
+	#if DEBUG_LISTS
+
+			cout << "Steps:" << SearchSteps << "\n";
+
+			int len = 0;
+
+			cout << "Open:\n";
+			MapSearchNode *p = astarsearch.GetOpenListStart();
+			while( p )
+			{
+				len++;
+	#if !DEBUG_LIST_LENGTHS_ONLY			
+				((MapSearchNode *)p)->PrintNodeInfo();
+	#endif
+				p = astarsearch.GetOpenListNext();
+				
+			}
+
+			cout << "Open list has " << len << " nodes\n";
+
+			len = 0;
+
+			cout << "Closed:\n";
+			p = astarsearch.GetClosedListStart();
+			while( p )
+			{
+				len++;
+	#if !DEBUG_LIST_LENGTHS_ONLY			
+				p->PrintNodeInfo();
+	#endif			
+				p = astarsearch.GetClosedListNext();
+			}
+
+			cout << "Closed list has " << len << " nodes\n";
+	#endif
+
+		}
+		while( SearchState == AStarSearch<MapSearchNode>::SEARCH_STATE_SEARCHING );
+
+		if( SearchState == AStarSearch<MapSearchNode>::SEARCH_STATE_SUCCEEDED )
+		{
+			cout << "Search found goal state\n";
+
+				MapSearchNode *node = astarsearch.GetSolutionStart();
+
+	#if DISPLAY_SOLUTION
+				cout << "Displaying solution\n";
+	#endif
+				int steps = 0;
+                                
+				node->PrintNodeInfo();
+				for( ;; )
+				{
+					node = astarsearch.GetSolutionNext();
+                    
+                    
+
+					if( !node )
+					{
+						break;
+					}
+
+					node->PrintNodeInfo();
+					steps ++;
+				
+				};
+
+				cout << "Solution steps " << steps << endl;
+
+				// Once you're done with the solution you can free the nodes up
+				astarsearch.FreeSolutionNodes();
+
+	
+		}
+		else if( SearchState == AStarSearch<MapSearchNode>::SEARCH_STATE_FAILED ) 
+		{
+			cout << "Search terminated. Did not find goal state\n";
+		
+		}
+
+		// Display the number of loops the search went through
+		cout << "SearchSteps : " << SearchSteps << "\n";
+
+		SearchCount ++;
+
+		astarsearch.EnsureMemoryFreed();
+	}
+	
+	Display_grid();
+    
+	
+    glutMainLoop();
+	
+	return 0;
 }
+
+
+
+
+
